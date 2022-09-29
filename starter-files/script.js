e7aa13cd696994edcfe112196bcc85fd2ab8f4ef
OsvaldoReTo <resendiz.tolentino.osvaldo@gmail.com>
2022-09-29 16:22:39 -0500

    chore | Se agregó función fetch con llamada a API

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,5 +1,34 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
+const btnResponse = document.getElementById("button");
+const question = document.getElementById("input");
+const Respuesta = document.getElementById("answer");
+
+
+btnResponse.addEventListener("click", function (e) {
+    e.preventDefault();
+    getResponse();
+})
+
+function getResponse() {
+    let promise = fetch(API_ENDPOINT,{
+        method: "GET"});
+        promise.then((response)=>{
+            response.json().then( (data)=>{
+                console.log(data);
+                Respuesta.innerHTML+=`${data.answer}`
+                setTimeout(() => {
+                     Respuesta.innerHTML=""
+                }, 3000);
+            }).catch((error) =>{
+                console.error(error);
+            })//catch
+        }).catch( (error) =>{
+            alert("Error"+error)
+
+        })
+        //promesa1
+};
 
 /**
  * STEPS:
